427fd69d7c969bcb6e503be8043aa8391baeb446
Ian Cullum <ian.p.cullum@gmail.com>
2022-06-11 14:16:09 -0700

    Implemented door spawning

--- Dungeon.gd
+++ Dungeon.gd
@@ -40,18 +40,20 @@ func spawn_dungeon(bsp:BSPNode, min_ext:Vector2, max_ext:Vector2):
 		spawn_dungeon(bsp.child1, min_ext, l_mid_ext)
 		spawn_dungeon(bsp.child2, r_mid_ext, max_ext)
 		print("\nl: ", min_ext, "\nl_mid: ", l_mid_ext, "\nr_mid: ", r_mid_ext, "\nr: ", max_ext)
-		spawn_door(ax_idx, min_ext, max_ext)
+		spawn_door(ax_idx, r_mid_ext, l_mid_ext)
 	else:
 		too_small(max_ext, min_ext)
 		Counters.count("leaves")
 		add_csg(csg_body, min_ext+wallThick, max_ext-wallThick, height, OP_SUB)
 
-func spawn_door(axis, rm_min_ext, rm_max_ext):
+func spawn_door(axis:int, rm_min_ext:Vector2, rm_max_ext:Vector2):
 	var mid_point:Vector2 = (rm_min_ext + rm_max_ext) / 2
 	var offset:Vector2 = Vector2()
-	offset[axis] = wallThick[axis]
+	offset[axis] = wallThick[axis] + 0.01
 	offset[(axis+1)%2] = 1
-	add_csg(csg_body, mid_point-offset, mid_point+offset , 2, OP_SUB)
+	var hght:float = height*2/3
+	var v_off:float = (hght-height)/2
+	add_csg(csg_body, mid_point-offset, mid_point+offset , hght, OP_SUB, v_off)
 
 func too_small(high_ext:Vector2, low_ext:Vector2):
 	if high_ext.x - low_ext.x < 1 or high_ext.y - low_ext.y < 1:
@@ -59,16 +61,16 @@ func too_small(high_ext:Vector2, low_ext:Vector2):
 	else:
 		Counters.increment("just_right")
 
-func add_csg(parent:Node, min_ext:Vector2, max_ext:Vector2, hgt:float, op:int=OP_UN):
+func add_csg(parent:Node, min_ext:Vector2, max_ext:Vector2, hgt:float, op:int=OP_UN, v_offset:float=0):
 	var size:Vector2 = max_ext - min_ext
 	var pos:Vector2 = min_ext + (size/2)
-	var csg = make_csg(pos, size, hgt, op)
+	var csg = make_csg(pos, size, hgt, op, v_offset)
 	parent.add_child(csg)
 	return csg
 
-func make_csg(pos, size, hgt, op=OP_UN, use_col=true):
+func make_csg(pos, size, hgt, op=OP_UN, v_offset:float=0, use_col=true):
 	var csg:CSGShape = CSGBox.new()
-	csg.translation = Vector3(pos.x, 0, pos.y)
+	csg.translation = Vector3(pos.x, v_offset, pos.y)
 	csg.width = size.x
 	csg.height = hgt
 	csg.depth = size.y
